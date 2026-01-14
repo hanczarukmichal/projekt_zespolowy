6db19b0feff1ac8bc11337e639ae35a4bcab9c10
hanczarukmichal <hanczarukmichal@gmail.com>
2026-01-14 23:21:02 +0100

    Dodano zakładkę z kursami walut pobieranymi z NBP

---
+++ projekt_zespolowy/Models/MarketDataViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace projekt_zespolowy.Models
+{
+    public class MarketDataViewModel
+    {
+        public List<ExchangeRate> Currencies { get; set; } = new List<ExchangeRate>();
+        public List<StockIndex> Indices { get; set; } = new List<StockIndex>();
+    }
+
+    public class ExchangeRate
+    {
+        public string Code { get; set; }
+        public string Currency { get; set; }
+        public decimal Mid { get; set; }
+    }
+
+    public class StockIndex
+    {
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public string Change { get; set; }
+    }
+}
